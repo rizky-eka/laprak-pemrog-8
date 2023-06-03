c2e6b445882eff59e7ddfd66ac3fd0741255e3c4
RIZKY EKA HARYADI <rizky28eka@MacBook-Pro-RIZKY.local>
2023-06-03 16:31:22 +0700

    oop inheritance

---
+++ Pertemuan8/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Pertemuan8
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string isName, int isAge)
+        {
+            this.Name = isName;
+            this.Age = isAge;
+
+            Console.WriteLine("Nama saya : {0} \nUmur saya :  {1} tahun", Name, Age);
+            Console.WriteLine();
+        }
+
+        public void infoPerson()
+        {
+            Console.WriteLine("Nama Saya {0}, dan umur saya {1} tahun", Name, Age);
+            Console.WriteLine();
+        }
+    }
+
+    public class Karyawan : Person
+    {
+        public int karyawanID { get; set; }
+        public string subject { get; set; }
+
+        public Karyawan()
+        {
+
+        }
+
+        public Karyawan(string subject)
+        {
+            this.subject = subject;
+            Console.WriteLine("Saya disini sebagai {0}", this.subject);
+            Console.WriteLine();
+        }
+        public void infoKaryawan()
+        {
+            Console.WriteLine("ID Karyawan : {0} \nNama Karyawan : {1}", karyawanID, subject);
+            Console.WriteLine();
+        }
+
+    }
+
+}
---
+++ Pertemuan8/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Pertemuan8
+{
+    public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write('\n');
+        Person person1 = new Person();
+        person1.Name = "Rizky Eka Haryadi";
+        person1.Age = 19;
+        person1.infoPerson();
+
+        Person person2 = new Person("Eka", 19);
+
+
+        Karyawan karyawan1 = new Karyawan();
+        karyawan1.karyawanID = 22114829;
+        karyawan1.subject = "Rizky Eka";
+        karyawan1.infoKaryawan();
+
+        Karyawan karyawan2 = new Karyawan("Mobile Developemnt");
+    }
+}
+}
